dc4e90ade04e5b632ff7e059d4ffcfcb41a0e4b4
Per Ploug <per.ploug@zalando.de>
2019-02-14 16:03:51 +0100

    Index.ts to use the testable classes
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import { Application } from "probot";
-
+import { Application, Context } from "probot";
+import { AppConfig, getTasksConfig } from "./config/app"
+import { Zincr } from "./zincr";
 
 export = (app: Application) => {
   
-  const handlePullRequest = require("./pull-request-change");
   const setStatusPass = require("./set-status-pass");
-
-  app.router
+  const events = ["pull_request", "pull_request_review"];
+  
   // Runs the check on all pull request and review events
-  app.on(
-    [
-      "pull_request", "pull_request_review"
-    ],
-    handlePullRequest
-  );
-
+  app.on(events, processPullRequest);
   app.on('check_run.requested_action', setStatusPass)
+
+  async function processPullRequest(context : Context) {
+    const repo = context.repo();
+    const config = await getTasksConfig(context);
+    var zincr = new Zincr(AppConfig, config, repo);
+    await zincr.onChange(context); 
+  }
 };
