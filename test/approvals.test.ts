9e7db41ad97f0a3002ccd996c5088dac91e7dba7
Per Ploug <per.ploug@zalando.de>
2019-02-21 15:25:20 +0100

    Tests to support commit lookup changes
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

--- test/approvals.test.ts
+++ test/approvals.test.ts
@@ -5,7 +5,9 @@ import { GitHubAPI } from "probot/lib/github";
 import nock = require("nock");
 import { StatusEnum } from "../src/interfaces/StatusEnum";
 
+
 nock.disableNetConnect();
+jest.setTimeout(10000);
 
 let task : Approvals, context : Context;
 
@@ -36,11 +38,13 @@ describe("zincr approvals", () => {
     done();
   })
 
+  
+  
   test("No reviews returns failure", async done => {
 
     nock("https://api.github.com")
-      .get("/repos/robotland/test/pulls/113/commits")
-      .reply(200,  [] );
+      .get("/repos/robotland/test/compare/607c64cd8e37eb2db939f99a17bee5c7d1a90a31...e76ed6025cec8879c75454a6efd6081d46de4c94")
+      .reply(200,  {commits: [] });
 
     nock("https://api.github.com")
       .get("/repos/robotland/test/pulls/113/reviews")
@@ -52,7 +56,7 @@ describe("zincr approvals", () => {
   
     done();
   })
-
+  
   test("2 reviews for non-employee returns success", async done => {
 
     nock("https://api.github.com")
@@ -60,8 +64,8 @@ describe("zincr approvals", () => {
       .reply(404);
 
     nock("https://api.github.com")
-      .get("/repos/robotland/test/pulls/113/commits")
-      .reply(200,  [] );
+      .get("/repos/robotland/test/compare/607c64cd8e37eb2db939f99a17bee5c7d1a90a31...e76ed6025cec8879c75454a6efd6081d46de4c94")
+      .reply(200,  {commits: [] });
 
     nock("https://api.github.com")
       .get("/repos/robotland/test/pulls/113/reviews")
@@ -81,9 +85,10 @@ describe("zincr approvals", () => {
       .get("/orgs/robotland/memberships/bkeepers")
       .reply(404);
 
+
     nock("https://api.github.com")
-      .get("/repos/robotland/test/pulls/113/commits")
-      .reply(200,  [] );
+      .get("/repos/robotland/test/compare/607c64cd8e37eb2db939f99a17bee5c7d1a90a31...e76ed6025cec8879c75454a6efd6081d46de4c94")
+      .reply(200,  {commits: []} );
 
     nock("https://api.github.com")
       .get("/repos/robotland/test/pulls/113/reviews")
@@ -97,6 +102,7 @@ describe("zincr approvals", () => {
     done();
   })
 
+  
   test("1 review + employee returns success", async done => {
 
     nock("https://api.github.com")
@@ -104,8 +110,8 @@ describe("zincr approvals", () => {
       .reply(200,  {state: "active"} );
 
     nock("https://api.github.com")
-      .get("/repos/robotland/test/pulls/113/commits")
-      .reply(200,  [] );
+      .get("/repos/robotland/test/compare/607c64cd8e37eb2db939f99a17bee5c7d1a90a31...e76ed6025cec8879c75454a6efd6081d46de4c94")
+      .reply(200,  {commits: []} );
 
     nock("https://api.github.com")
       .get("/repos/robotland/test/pulls/113/reviews")
@@ -117,6 +123,7 @@ describe("zincr approvals", () => {
 
     done();
   })
+  
 
   test("review by co-author returns failure", async done => {
 
@@ -125,8 +132,8 @@ describe("zincr approvals", () => {
       .reply(200,  {state: "active"} );
 
     nock("https://api.github.com")
-      .get("/repos/robotland/test/pulls/113/commits")
-      .reply(200,  [{author: {login: "a"}, commit: { message: 'change' }}] );
+      .get("/repos/robotland/test/compare/607c64cd8e37eb2db939f99a17bee5c7d1a90a31...e76ed6025cec8879c75454a6efd6081d46de4c94")
+      .reply(200,  {commits: [{author: {login: "a"}, commit: {committer: {login: "a"}, message: 'change' }}]} );
 
     nock("https://api.github.com")
       .get("/repos/robotland/test/pulls/113/reviews")
@@ -135,7 +142,7 @@ describe("zincr approvals", () => {
     nock("https://api.github.com")
       .put("/repos/robotland/test/pulls/113/reviews/666/dismissals")
       .reply(200,  {} );
-
+      
     await task.run(context);
     
     expect(task.result.length).toBe(1);
@@ -143,7 +150,7 @@ describe("zincr approvals", () => {
 
     done();
   })
-
+ 
   test("review by co-author and non-author returns success", async done => {
 
     nock("https://api.github.com")
@@ -151,8 +158,8 @@ describe("zincr approvals", () => {
       .reply(200,  {state: "active"} );
 
     nock("https://api.github.com")
-      .get("/repos/robotland/test/pulls/113/commits")
-      .reply(200,  [{author: {login: "a"}, commit: { message: 'change' }}] );
+      .get("/repos/robotland/test/compare/607c64cd8e37eb2db939f99a17bee5c7d1a90a31...e76ed6025cec8879c75454a6efd6081d46de4c94")
+      .reply(200,  {commits:  [{author: {login: "a"}, commit: {committer: {login: "a"}, message: 'change' }}]} );
 
     nock("https://api.github.com")
       .get("/repos/robotland/test/pulls/113/reviews")
@@ -167,4 +174,6 @@ describe("zincr approvals", () => {
     
     done();
   })
+  
+
 });
