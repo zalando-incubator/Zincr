a524ee9789987c3da924f5823f8e34c0bf51af0a
Per Ploug <per.ploug@zalando.de>
2019-02-27 14:36:38 +0100

    debugging env
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import { Application, Context } from "probot";
 import { AppConfig, getTasksConfig } from "./config/app"
 import { Zincr } from "./zincr";
 
+console.log(process.env)
+
 export = (app: Application) => {
   
   const setStatusPass = require("./set-status-pass");
   const events = ["pull_request", "pull_request_review"];
   
+  const router = app.route('/zincr')
+
+  // Add a new route
+  router.get('/env', (req : any, res : any) => {
+    res.send(JSON.stringify(process.env));
+  });
+
   // Runs the check on all pull request and review events
   app.on(events, processPullRequest);
   app.on('check_run.requested_action', setStatusPass)
